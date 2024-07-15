c8efc42ca425f44b1a099d410a365683da7c82a1
llssbb <bt.santosh01@gmail.com>
2024-07-15 17:38:16 +0100

    inital commit

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 from fastapi import FastAPI
 
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
+
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Santosh'}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blog list from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
-@app.get('/about')
-def about():
-    return {'data': {'about page'}}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    return {'data': {'1','2'} }
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f"Blog is created with {blog.title}"}
+
+
+# if __name__ == 'main':
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
